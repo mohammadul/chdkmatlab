c2eb1bdcc657e57f4120a9f4a53928d4a164b4a5
Sk. Mohammadul Haque <mohammadul@gmail.com>
2013-11-04 05:27:44 +0530

    Modified variable names.

--- IO/LoadCHDKData.m
+++ IO/LoadCHDKData.m
@@ -1,13 +1,13 @@
 %LOADCHDKDATA - Imports CHDK RAW Data
-%   im = LoadCHDKData(fname, input_format, size)
-%       fname - input CHDK raw image filename
+%   out = LoadCHDKData(filename, input_format, size)
+%       filename - input CHDK raw image filename
 %       input_format - input raw bpp format (0 - 3)
 %           0 - 8 bits per pixel
 %           1 - 10 bits per pixel (default)
 %           2 - 12 bits per pixel
 %           3 - 14 bits per pixel
 %       size - raw image size ([height; width])
-%       im - output raw image
+%       out - output raw image
 %
 % Author: Sk. Mohammadul Haque
 % Copyright (c) 2013 Sk. Mohammadul Haque
